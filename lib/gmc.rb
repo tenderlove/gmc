514d5d2b8ca992c5ca91a2272668fbd27940a87d
Aaron Patterson <aaron.patterson@gmail.com>
2017-06-08 18:43:21 -0700

    Add history parser
    This commit adds the history parser and makes opening the tty more
    stable (and reading the history data more stable)

--- lib/gmc.rb
+++ lib/gmc.rb
@@ -1,5 +1,6 @@
 require 'termios'
 require 'fcntl'
+require 'strscan'
 
 class GMC
   class TTY
@@ -89,9 +90,159 @@ class GMC
 
     # Try reading the version a few times before giving up
     gmc.flush
+    2.times { gmc.version }
     return gmc if gmc.version
 
     raise "Couldn't open device"
+  end
+
+  class SampleInfo
+    def initialize count_frequency, sample_frequency
+      @count_frequency  = count_frequency
+      @sample_frequency = sample_frequency
+    end
+
+    def calc_sample_time start, offset
+      start + (offset * @sample_frequency)
+    end
+  end
+
+  class CPS < SampleInfo
+    def initialize
+      super(1, 1)
+    end
+
+    def to_cpm val
+      node = val
+      count = 0
+      60.times do
+        break unless node
+        count += node.count
+        node = node.prev
+      end
+      count
+    end
+
+    def to_cps val
+      val.count
+    end
+  end
+
+  class CPM < SampleInfo
+    def initialize sample_frequency
+      super(60, 1)
+    end
+
+    def to_cpm val
+      val
+    end
+
+    def to_cps val
+      nil
+    end
+  end
+
+  class Sample
+    attr_reader :prev, :count
+    attr_accessor :next
+
+    include Enumerable
+
+    def initialize start_time, offset, count, info, prev
+      @start_time = start_time
+      @offset     = offset
+      @count      = count
+      @info       = info
+      @prev       = prev
+      @next       = nil
+      @cpm        = nil
+    end
+
+    def each
+      n = self
+      while n
+        yield n
+        n = n.next
+      end
+    end
+
+    def cpm
+      return @cpm if @cpm
+      @cpm = @info.to_cpm self
+    end
+
+    def cps
+      @info.to_cps self
+    end
+
+    # uSv / h
+    def uSv
+      # Apparently for the GMC-320+, 200 cpm = 1.0 uSv / h
+      # https://www.gqelectronicsllc.com/forum/topic.asp?TOPIC_ID=4037
+      cpm / 200.0
+    end
+
+    def to_ary
+      sample_time = @info.calc_sample_time @start_time, @offset
+      [sample_time, cps, cpm, uSv]
+    end
+  end
+
+  CPS_PS = CPS.new      # count per sec, recorded each sec
+  CPM_PM = CPM.new 60   # count per min, recorded each min
+  CPM_PH = CPM.new 3600 # count per min, recorded each hr
+
+  def self.parse_history data
+    buf     = StringScanner.new data
+    head    = nil
+    samples = nil
+
+    while !buf.eos?
+      byte = buf.get_byte
+      case byte
+      when 'U'.b
+        if buf.peek(1) == "\xAA".b
+          # Some kind of command
+          buf.get_byte
+          x = buf.get_byte
+          case x
+          when "\x00".b # date / timestamp + history type
+            time = 6.times.map { buf.get_byte.ord }
+            time[0] += 2000
+            last_time = Time.mktime(*time)
+            offset    = 0
+            _55, _AA, history_type = *3.times.map { buf.get_byte }
+            if _55 == "\x55".b && _AA == "\xAA".b
+              sample_info = case history_type.ord
+                            when 1
+                              CPS_PS
+                            when 2
+                              CPM_PM
+                            when 3
+                              CPM_PH
+                            else
+                              raise "Unknown history type: %d" % [history_type.ord]
+                            end
+            else
+              raise "format error"
+            end
+          when "\x01".b # double byte sample (sample that exceeds 255)
+            raise "big"
+          else
+            raise "Unkown command %d" % x.bytes
+          end
+        end
+      when "\xFF".b
+      else
+        offset += 1
+        samples = Sample.new(last_time, offset, byte.ord, sample_info, samples)
+        if samples.prev
+          samples.prev.next = samples
+        end
+        head ||= samples
+      end
+    end
+    head
   end
 
   def initialize tty
@@ -137,27 +288,35 @@ class GMC
     buf = ''.b
 
     (0..0x0F0000).step(0x1000).each do |i|
-      3.times do |retries|
-        @tty.write "<SPIR"
-        z = [(0xFF0000 & i) >> 16, (0x00FF00 & i) >> 8, 0x0000FF & i]
-        @tty.write z.pack('C3')
-        @tty.write [0x0f, 0xff].pack('C2')
-        @tty.write ">>"
-        buf_slice = @tty.read 4096
+      @tty.write "<SPIR"
+      z = [(0xFF0000 & i) >> 16, (0x00FF00 & i) >> 8, 0x0000FF & i]
+      @tty.write z.pack('C3')
+      @tty.write [0x0f, 0xff].pack('C2')
+      @tty.write ">>"
+      buf_slice = @tty.read 4096
 
-        if buf_slice.nil?
-          $stderr.puts "retrying"
+      if buf_slice.nil? || buf_slice.empty?
+        $stderr.puts "retrying"
+        return self.history
+      else
+        if buf_slice.bytes.all? { |byte| byte == 255 }
+          $stderr.puts "FINISHED"
+          break
         else
-          if buf_slice.bytes.all? { |byte| byte == 255 }
-            $stderr.puts "FINISHED"
-            return buf
-          else
-            buf << buf_slice
-            break
-          end
+          buf << buf_slice
         end
       end
     end
+
+    if buf.empty?
+      self.history
+    else
+      buf
+    end
+  end
+
+  def samples
+    GMC.parse_history history
   end
 
   def serial
